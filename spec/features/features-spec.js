2a0f2afb2b5323398c0195a9ab2e0737731b5a0e
jebax <joel.eliot.baxter@gmail.com>
2018-12-06 15:16:10 +0000

    Added Game and high-level feature test
    Tests for initializing a game and playing 5 moves to determine X as the winner. Added Game class to avoid jasmine errors.

---
+++ spec/features/features-spec.js
@@ -0,0 +1,20 @@
+describe('Features', () => {
+  var Game = require ('../../src/game')
+  var game
+
+  beforeEach(() => {
+    game = new Game()
+  })
+
+  describe('Playing a game', () => {
+    xit('results in a win for X when 0, 1, and 2 are filled with X', () => {
+      game.initialize()
+      game.addMove(0)
+      game.addMove(3)
+      game.addMove(1)
+      game.addMove(6)
+      game.addMove(2)
+      expect(game.calculateWinner()).toEqual('X')
+    })
+  })
+})
